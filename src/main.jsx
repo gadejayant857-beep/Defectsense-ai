59aeda53a0520f221392b8aa88936e0f9b875a1e
Jayant <gadejayant857@gmail.com>
2026-09-25 23:51:46 +0530

    Refresh production React bundle

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// DefectSense production boot v2
+
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props)
